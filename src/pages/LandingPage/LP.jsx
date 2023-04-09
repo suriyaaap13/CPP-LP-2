5d752d5c28e47c02c58144eaa7c9a89b62201eb7
Suriyaa <suriyaaap13@gmail.com>
2023-04-09 10:54:42 +0530

    Set up is going on

---
+++ src/pages/LandingPage/LP.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Hero from '../../components/Hero/Hero';
+import FAQs from '../../components/FAQs/FAQs';
+import Footer from '../../components/Footer/Footer';
+
+export default function LP() {
+  return (
+    <div>
+      
+      {/* Hero Section */}
+      <Hero/>
+
+      {/* Take Away Section */}
+
+      {/* Campus Leader video Section */}
+
+      {/* Partners Section */}
+
+      {/* Community Section */}
+
+
+      {/* FAQs */}
+
+      <FAQs/>
+
+
+      {/* Contact Us */}
+
+
+      {/* Footer */}
+      <Footer/>
+
+    </div>
+  )
+}
