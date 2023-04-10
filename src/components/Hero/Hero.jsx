6214f0836402183046f8eb774b6bea9b89e76fa9
Suriyaa <suriyaaap13@gmail.com>
2023-04-10 15:51:14 +0530

    Create the Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from 'react';
+import Styles from './Hero.module.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 
 export default function Hero() {
   return (
-    <div>
-      Hero
+    <div className={`${Styles.hero_bg} pt-3`}>
+      
+
+
+      {/* Navbar */}
+      <Navbar expand='lg'>
+        <Container>
+          <Navbar.Brand href='#'>
+            <Image
+              src='/img/Hero/az-logo-black.svg'
+              width={150}
+              className='img-fluid'
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='az-navbar' />
+          <Navbar.Collapse id='az-navbar'>
+            <Nav className={`${Styles.nav_links} ms-auto`}>
+              <Nav.Link href='#'>Lead Login</Nav.Link>
+              <Nav.Link href='#'>Student Login</Nav.Link>
+              <Nav.Link href='#'>Premium</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Hero Section */}
+      <div className='d-flex container justify-content-between'>
+        {/* Text Section */}
+        <div className='py-5 my-3'>
+          {/* Hero text */}
+          <div className={`${Styles.hero_text}`}>
+            <span>We l</span>
+            <span>
+              <Image
+                src='/img/Hero/heart.svg'
+              />ve
+            </span>
+            <p>Coding</p>
+          </div>
+          {/* Hero SubText */}
+          <div>
+          The <strong>AlgoZenith Campus Connection Program</strong> is designed to help <br/> students establish a community of like-minded individuals on their <br/> campus who are passionate about programming and technology.
+          </div>
+          {/* Buttons */}
+          <div className='mt-3'>
+            <Button className={`${Styles.create_chapter_btn} me-4`}>Create a chapter -&gt;</Button>
+            <Button className={`${Styles.view_partners_btn}`}>View all partners</Button>
+          </div>
+        </div>
+        {/* Flag Section */}
+        <div>
+          <Image
+            src='/img/Hero/flag_alone.png'
+            height={500}
+          />
+        </div>
+      </div>
     </div>
   )
 }
