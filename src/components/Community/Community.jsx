a7db2b4552d0f61f35a0f13c86cf01ee71ad197d
Suriyaa <suriyaaap13@gmail.com>
2023-04-13 00:40:34 +0530

    Made the comunity section responsive

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -6,8 +6,8 @@ export default function Community() {
   return (
     <div>
         <div className={`${Styles.page_gradient}`}>
-            <div className={`${Styles.title} text-center pt-4`}>Our Partners</div>
-            <div className={`${Styles.sub_title} text-center pt-4`}>At AlgoZenith, we believe in the power of community and collaboration. Through our partnerships, we have had the opportunity to organize and <br/> participate in various technical events, which have enabled students & professionals to come together, learn, collaborate, & showcase their skills.</div>
+            <div className={`${Styles.title} text-center pt-4 fs-1`}>Our Partners</div>
+            <div className={`${Styles.sub_title} text-center pt-4 fs-6`}>At AlgoZenith, we believe in the power of community and collaboration. Through our partnerships, we have had the opportunity to organize and <br/> participate in various technical events, which have enabled students & professionals to come together, learn, collaborate, & showcase their skills.</div>
             {/* Buttons */}
             <div className='text-center mt-5'>
                 <Button variant='dark' className={`${Styles.create_chapter_btn} me-4`}>Create a chapter -&gt;</Button>
@@ -25,32 +25,40 @@ export default function Community() {
         <div className='container text-center mt-5'>
             
             {/* Title */}
-            <div className={`${Styles.title}`}>
+            <h1 className={`${Styles.title} fs-1`}>
             A vibrant glimpse into the community
-            </div>
+            </h1>
 
             {/* Heading */}
-            <div className={`${Styles.sub_title}`}>
+            <div className={`${Styles.sub_title} mt-3 fs-6`}>
             Browse through the gallery to see images of coding competitions, workshops, guest lectures, & other events organized by different chapters <br/> across different campuses, and create one for your university as well!
             </div>
 
             {/* Stats */}
             <div className='my-3 d-flex justify-content-center align-items-center '>
-                <div className='p-5'>
-                    <div className={`${Styles.title}`}>100+</div>
-                    <div className={`${Styles.sub_heading}`}>chapters</div>
+                
+                <div className='px-5 py-3'>
+                    <h1 className={`${Styles.title} fs-1`}>100+</h1>
+                    <h6 className={`${Styles.sub_heading} fs-6`}>chapters</h6>
                 </div>
-                <div className='p-5 border-start border-end'>
-                    <div className={`${Styles.title}`}>5000+</div>
-                    <div className={`${Styles.sub_heading}`}>Students</div>
+                
+                <div className='px-5 py-3 border-start border-end'>
+                    <h1 className={`${Styles.title} fs-1`}>5000+</h1>
+                    <h6 className={`${Styles.sub_heading} fs-6`}>Students</h6>
                 </div>
-                <div className='p-5'>
-                    <div className={`${Styles.title}`}>200+</div>
-                    <div className={`${Styles.sub_heading}`}>Events organized</div>
+
+                <div className='px-5 py-3'>
+                    <h1 className={`${Styles.title} fs-1`}>200+</h1>
+                    <h6 className={`${Styles.sub_heading} fs-6`}>Events organized</h6>
                 </div>
+            
             </div>
+            
+            
             <Button variant='bg-light' className={`${Styles.create_chapter_btn} mt-3`}>Create a chapter -&gt;</Button>
 
+            
+            
             {/* Horizontal Scroll Images */}
             <div>
                 
