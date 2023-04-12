599a3f7b74cc54e4705fab8e6fa7cd7f691a624b
Suriyaa <suriyaaap13@gmail.com>
2023-04-13 00:10:32 +0530

    Made the CampusLeader Page responsive

--- src/components/CampusLeader/CampusLeader.jsx
+++ src/components/CampusLeader/CampusLeader.jsx
@@ -1,33 +1,87 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Styles from './CampusLeader.module.css';
 
 
 export default function CampusLeader() {
   return (
-    <Container className={`text-center`}>
-        <h1 className={`${Styles.title} py-1`}>Together, We Code Better</h1>
-        <h6 className={`${Styles.sub_title} py-1`}>Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.</h6>
-        <Container className='w-full d-flex flex-wrap justify-content-between px-5'>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/01.mp4' type="video/mp4" />
-          </video>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/02.mp4' type="video/mp4" />
-          </video>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/03.mp4' type="video/mp4" />
-          </video>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/04.mp4' type="video/mp4" />
-          </video>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/05.mp4' type="video/mp4" />
-          </video>
-          <video className={`rounded-3 shadow`} width="230" height="230" controls>
-            <source src='/video/Leaders/06.mp4' type="video/mp4" />
-          </video>
-        </Container>
-    </Container>
+    <div className='pb-5 mb-3'>
+      <Container className={`text-center`}>
+          <h1 className={`${Styles.title} py-1 fs-1`}>Together, We Code Better</h1>
+          <h6 className={`${Styles.sub_title} py-1 fs-6`}>Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.</h6>
+          <Container className='w-75 d-none d-md-grid gap-4 px-md-5 mt-4'>
+            
+            <Row className='d-none d-md-flex'>
+              
+              <Col>
+                <video className={`rounded-3 shadow`} width="230" height="230" controls>
+                  <source src='/video/Leaders/01.mp4' type="video/mp4" />
+                </video>
+              </Col>
+              
+              <Col>
+                <video className={`rounded-3 shadow`} width="230" height="230" controls>
+                  <source src='/video/Leaders/02.mp4' type="video/mp4" />
+                </video>
+              </Col>
+              
+              <Col>
+                <video className={`rounded-3 shadow`} width="230" height="230" controls>
+                  <source src='/video/Leaders/03.mp4' type="video/mp4" />
+                </video>
+              </Col>
+            
+            </Row>
+            
+            <Row className='d-none d-md-flex mx-md-5 px-md-5'>
+              
+              <Col>
+                <video className={`rounded-3 shadow`} width="230" height="230" controls>
+                  <source src='/video/Leaders/05.mp4' type="video/mp4" />
+                </video>
+              </Col>
+              
+              <Col>
+                <video className={`rounded-3 shadow`} width="230" height="230" controls>
+                  <source src='/video/Leaders/06.mp4' type="video/mp4" />
+                </video>
+              </Col>
+            </Row>
+
+            
+          
+          </Container>
+
+          
+          
+          
+      </Container>
+      <div className='vw-100 mt-3 d-flex d-md-none overflow-auto'>
+
+        <video className={`rounded-3 shadow mx-3`} width="230" height="230" controls>
+          <source src='/video/Leaders/01.mp4' type="video/mp4" />
+        </video>
+
+        <video className={`rounded-3 shadow mx-3`} width="230" height="230" controls>
+          <source src='/video/Leaders/02.mp4' type="video/mp4" />
+        </video>
+
+        <video className={`rounded-3 shadow mx-3`} width="230" height="230" controls>
+          <source src='/video/Leaders/03.mp4' type="video/mp4" />
+        </video>
+
+        <video className={`rounded-3 shadow mx-3`} width="230" height="230" controls>
+          <source src='/video/Leaders/05.mp4' type="video/mp4" />
+        </video>
+
+        <video className={`rounded-3 shadow mx-3`} width="230" height="230" controls>
+          <source src='/video/Leaders/06.mp4' type="video/mp4" />
+        </video>
+
+
+      </div>
+    </div>    
   )
 }
